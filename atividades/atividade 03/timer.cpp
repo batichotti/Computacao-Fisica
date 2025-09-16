3cf93dc4849f8edd7a0fe1a320b9022a764bb616
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-09-16 12:14:12 -0300

    Update timer.cpp

--- atividades/atividade 03/timer.cpp
+++ atividades/atividade 03/timer.cpp
@@ -1,7 +1,27 @@
-const unsigned char Digitos[] = {0x08, 0x10, 0x20, 0x04};
-const unsigned char Tabela[] = {0x40, 0x79, 0x24, 0x30, 0x19, 0x12, 0x02, 0x78, 0x00, 0x18};
+/* Falta:
+- Buzzer ativo por 300ms ao terminar tempo do cronometro;
+- Piscar dois pontos durante a contagem regressiva;
+- Configurar minuto e segundo de forma separada ao segurar o botao por xxms; *
+- Avancar mais rapidamente o tempo ao girar mais rapido. *
+
+*(Extra)
+
+*/
+
+char Posicao[][2] = {{0x08, 0}, {0x10, 0}, {0x20, 0}, {0x04, 0}}; // {QUEM, O QUE} DM UM : DS US
+const unsigned char Numero[] = {0x40, 0x79, 0x24, 0x30, 0x19, 0x12, 0x02, 0x78, 0x00, 0x10};
 int alg = 0;
 long long int tempo = 0;
+long long int buzzer = 0;
+
+unsigned long clique = 0;
+char lastCLK;
+char lastSW;
+char lastDT;
+int cronometro = 0;
+bool press = 1;
+int doisPontos = 0;
+bool doisPOn = 1;
 
 void setup() {
   DDRB &= ~(1<<PB0);
@@ -37,13 +57,114 @@ void setup() {
   DDRC |= (1<<PC5);
 }
 
-void loop() {
-  PORTD = Tabela[0];
-
-  if((millis() - tempo) > 5){
+void pisca(int piscada){
+  if((millis() - tempo) > piscada){
     PORTC &= ~(PORTC);
-    PORTC = Digitos[alg++];
+    PORTD = Numero[Posicao[alg][1]];
+    PORTC = Posicao[alg++][0];
     alg %= 4;
     tempo = millis();
   }
+}
+
+void acres(){
+  Posicao[3][1]++;
+
+  if(Posicao[3][1] >= 10){
+    Posicao[3][1] %= 10;
+    Posicao[2][1]++;
+
+    if(Posicao[2][1] >= 6){
+      Posicao[2][1] %= 6;
+      Posicao[1][1]++;
+            
+      if(Posicao[1][1] >= 10){
+        Posicao[1][1] %= 10;
+        Posicao[0][1]++;
+        
+        if(Posicao[0][1] >= 6) Posicao[0][1] %= 6;
+      }
+    }
+  }
+}
+
+void decres(){
+  Posicao[3][1]--;
+        
+  if(Posicao[3][1] < 0){
+    Posicao[3][1] = 9;
+    Posicao[2][1]--;
+          
+    if(Posicao[2][1] < 0){
+      Posicao[2][1] = 5;
+      Posicao[1][1]--;
+            
+      if(Posicao[1][1] < 0){
+        Posicao[1][1] = 9;
+        Posicao[0][1]--;
+        if(Posicao[0][1] < 0) Posicao[0][1] = 5;
+      }
+    }
+  }
+}
+
+void loop(){
+  char CLK = PINB & (1<<PB0);
+  char DT = PINB & (1<<PB1);
+  
+  static unsigned long CLKTime = 0;
+  const unsigned long debounce = 5;
+
+  if (CLK != lastCLK && (millis() - CLKTime) > debounce){
+    CLKTime = millis();
+
+    if (CLK == 0){
+
+      if (DT != 0){ // Horário
+        acres();
+
+      } else { // Anti-horário
+        decres();
+      }
+    }  
+  }
+  lastCLK = CLK;
+  lastDT = DT;
+
+  char SW = PINB & (1<<PB2);
+
+  if (SW == 0 && lastSW != 0 && (millis() - clique) > 50) {
+    clique = millis();
+    press = !press;
+  }
+
+  if (!press) {
+    if ((millis() - cronometro) > 1000) {
+        decres();
+        cronometro = millis();
+
+        if((millis() - doisPontos) > 500){
+          if(doisPOn) PORTB |= (1<<PB3);
+          else PORTB &= ~(1<<PB3);
+          doisPOn = !doisPOn;
+
+          doisPontos = millis();
+        }
+
+        if(Posicao[0][1] == 0 && Posicao[1][1] == 0 && 
+        Posicao[2][1] == 0 && Posicao[3][1] == 0){
+          press = !press;
+          PORTB &= ~(1<<PB4);
+          buzzer = millis();
+
+          PORTB &= ~(1<<PB3);
+          doisPOn = 1;
+        }
+    }
+  }
+
+  if((millis() - buzzer) > 300 && buzzer != 0) PORTB |= (1<<PB4);
+
+  lastSW = SW;
+  pisca(5);
 }
